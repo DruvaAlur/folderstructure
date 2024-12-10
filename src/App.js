6b32e2555461bbfa60c4ef619e086c0732012c80
Druva Alur <druva.alur@gmail.com>
2024-12-10 09:53:18 +0530

    file exists

--- src/App.js
+++ src/App.js
@@ -87,12 +87,13 @@ function App() {
 
   const checkIfAlreadyExists = (node, newName) => {
     const nodeKeys = Array.isArray(node) ? node : Object.keys(node);
-
-    if (nodeKeys.some((key) => key.toLowerCase() === newName.toLowerCase())) {
-      alert(
-        "A file/folder already exists with the same name. Please choose another name."
-      );
-      return true;
+    for (let key of nodeKeys) {
+      if (key.toLowerCase() === newName.toLowerCase()) {
+        alert(
+          "A file/folder already exists with the same name. Please choose another name."
+        );
+        return true;
+      }
     }
 
     return false;
